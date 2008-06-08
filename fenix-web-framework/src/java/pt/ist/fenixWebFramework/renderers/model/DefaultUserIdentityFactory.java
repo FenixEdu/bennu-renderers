345d1b0ef6c2a45abf969e2ad27bec845a6bf437
Luis Cruz <luis.cruz@ist.utl.pt>
2008-06-08 17:11:17 +0000

    Don't create a session if it doesn't exist.

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/model/DefaultUserIdentityFactory.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/model/DefaultUserIdentityFactory.java
@@ -1,13 +1,14 @@
 package pt.ist.fenixWebFramework.renderers.model;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 public class DefaultUserIdentityFactory extends UserIdentityFactory {
 
     @Override
-    public UserIdentity createUserIdentity(HttpServletRequest request) {
-        String sessionId = request.getSession() != null ? request.getSession().getId() : null;
-        
+    public UserIdentity createUserIdentity(final HttpServletRequest request) {
+	final HttpSession httpSession = request.getSession(false);
+        final String sessionId = httpSession == null ? null : httpSession.getId();
         return new SimpleUserIdentity(sessionId);
     }
 
