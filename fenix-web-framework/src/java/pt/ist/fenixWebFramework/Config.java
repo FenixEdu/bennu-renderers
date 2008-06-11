4244549b726316e3d5f7ae06ada17152df59af99
Luis Cruz <luis.cruz@ist.utl.pt>
2008-06-11 14:53:34 +0000

    Initialize profile logging in FenixWebFramework thus avoiding property substitution at compile time.

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/Config.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/Config.java
@@ -25,6 +25,21 @@ public class Config extends pt.ist.fenixframework.Config {
     protected String defaultVariant = null;
 
 
+    /**
+     * This <strong>optional</strong> parameter specifies the directory where the profile
+     * log will be stored. If it is not specified, and a filename is provided for the log,
+     * the current working directory will be used.
+     */
+    protected String logProfileDir = null;
+
+    /**
+     * This <strong>optional</strong> parameter specifies the filename where the profile
+     * log will be stored. If it is not specified, the profiling logger will not be
+     * initialized, and it will fall back on whatever is specified in to log4j.xml file.
+     */
+    protected String logProfileFilename = null;
+
+
     // TODO : make method in superclass visible in the hierarchy so we don't need to replicate the code here.
     private static void checkRequired(Object obj, String fieldName) {
         if (obj == null) {
--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/FenixWebFramework.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/FenixWebFramework.java
@@ -1,6 +1,17 @@
 package pt.ist.fenixWebFramework;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Locale;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.Priority;
+import org.apache.log4j.varia.DenyAllFilter;
+import org.apache.log4j.varia.LevelRangeFilter;
 
 import pt.ist.fenixframework.FenixFramework;
 import pt.utl.ist.fenix.tools.util.i18n.Language;
@@ -10,17 +21,47 @@ public class FenixWebFramework extends FenixFramework {
 
     private static final Object INIT_LOCK = new Object();
 
-    public static void initialize(Config config) {
-        synchronized (INIT_LOCK) {
-            FenixFramework.initialize(config);
+    public static void initialize(final Config config) {
+	synchronized (INIT_LOCK) {
+	    FenixFramework.initialize(config);
 
-            final Locale locale = new Locale(config.getDefaultLanguage(), config.getDefaultLocation(), config.getDefaultVariant());
-            Language.setDefaultLocale(locale);
-        }
+	    final Locale locale = new Locale(config.getDefaultLanguage(), config.getDefaultLocation(), config.getDefaultVariant());
+	    Language.setDefaultLocale(locale);
+
+	    initializeLoggingSystem(config);
+	}
+    }
+
+    private static void initializeLoggingSystem(final Config config) {
+	if (config.logProfileFilename != null) {
+	    final Logger logger = Logger.getLogger("com.atlassian.util.profiling");
+	    logger.setAdditivity(false);
+
+	    final Layout layout = new PatternLayout("%d{HH:mm:ss.SSS} %m%n");
+	    final String filename = config.logProfileDir == null ? config.logProfileFilename : config.logProfileDir
+		    + File.separatorChar + config.logProfileFilename;
+	    try {
+		final FileAppender fileAppender = new FileAppender(layout, filename, true);
+		fileAppender.setName("com.atlassian.util.profiling");
+		fileAppender.setThreshold(Priority.INFO);
+
+		final LevelRangeFilter levelRangeFilter = new LevelRangeFilter();
+		levelRangeFilter.setLevelMin(Level.INFO);
+		levelRangeFilter.setLevelMax(Level.WARN);
+		levelRangeFilter.setAcceptOnMatch(true);
+		fileAppender.addFilter(levelRangeFilter);
+		final DenyAllFilter denyAllFilter = new DenyAllFilter();
+		fileAppender.addFilter(denyAllFilter);
+
+		logger.addAppender(fileAppender);
+	    } catch (IOException ex) {
+		throw new Error(ex);
+	    }
+	}
     }
 
     public static Config getConfig() {
-        return (Config) FenixFramework.getConfig();
+	return (Config) FenixFramework.getConfig();
     }
 
     public static DomainModel getDomainModel() {
