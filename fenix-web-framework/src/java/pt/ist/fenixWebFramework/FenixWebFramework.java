00dd29e10b4642fd675892e140c91bd2b9e04807
Luis Cruz <luis.cruz@ist.utl.pt>
2008-06-16 13:42:03 +0000

    Don't make initialization of file manager manditory.fenix-web-framework/src/java/pt/ist/fenixWebFramework/FenixWebFramework.java sandbox/myWebApp/web/WEB-INF/lib/fenix-web-framework.jar fenix/web/WEB-INF/lib/fenix-web-framework.jar

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/FenixWebFramework.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/FenixWebFramework.java
@@ -69,29 +69,29 @@ public class FenixWebFramework extends FenixFramework {
     }
 
     private static void initializeFileManager(Config config) {
+	if (config.getDspaceClientTransportClass() != null) {
+	    final Properties properties = new Properties();
+	    properties.put("dspace.client.transport.class", config.getDspaceClientTransportClass());
+	    properties.put("file.manager.factory.implementation.class", config.getFileManagerFactoryImplementationClass());
+	    properties.put("dspace.serverUrl", config.getDspaceServerUrl());
+	    properties.put("dspace.downloadUriFormat", config.getDspaceDownloadUriFormat());
+	    properties.put("dspace.username", config.getDspaceUsername());
+	    properties.put("dspace.password", config.getDspacePassword());
+	    properties.put("dspace.rmi.server.name", config.getDspaceRmiServerName());
+	    properties.put("jndi.properties.file", config.getJndiPropertiesFile());
+	    properties.put("rmi.registry.port", config.getRmiRegistryPort());
+	    properties.put("rmi.port", config.getRmiPort());
+	    properties.put("rmi.ssl", config.getRmiSsl());
+	    properties.put("rmi.ssl.truststore", config.getRmiSslTruststore());
+	    properties.put("rmi.ssl.truststore.password", config.getRmiSslTruststorePassword());
+	    properties.put("rmi.stream.bytes.min", config.getRmiStreamBytesMin());
+	    properties.put("rmi.stream.bytes.max", config.getRmiStreamBytesMax());
+	    properties.put("rmi.stream.bytes.block", config.getRmiStreamBytesBlock());
 
-	final Properties properties = new Properties();
-	properties.put("dspace.client.transport.class", config.getDspaceClientTransportClass());
-	properties.put("file.manager.factory.implementation.class", config.getFileManagerFactoryImplementationClass());
-	properties.put("dspace.serverUrl", config.getDspaceServerUrl());
-	properties.put("dspace.downloadUriFormat", config.getDspaceDownloadUriFormat());
-	properties.put("dspace.username", config.getDspaceUsername());
-	properties.put("dspace.password", config.getDspacePassword());
-	properties.put("dspace.rmi.server.name", config.getDspaceRmiServerName());
-	properties.put("jndi.properties.file", config.getJndiPropertiesFile());
-	properties.put("rmi.registry.port", config.getRmiRegistryPort());
-	properties.put("rmi.port", config.getRmiPort());
-	properties.put("rmi.ssl", config.getRmiSsl());
-	properties.put("rmi.ssl.truststore", config.getRmiSslTruststore());
-	properties.put("rmi.ssl.truststore.password", config.getRmiSslTruststorePassword());
-	properties.put("rmi.stream.bytes.min", config.getRmiStreamBytesMin());
-	properties.put("rmi.stream.bytes.max", config.getRmiStreamBytesMax());
-	properties.put("rmi.stream.bytes.block", config.getRmiStreamBytesBlock());
-
-	FileManagerFactory.init(config.getFileManagerFactoryImplementationClass());
-
-	DSpaceFileManagerFactory.init(properties);
-
+	    FileManagerFactory.init(config.getFileManagerFactoryImplementationClass());
+	    
+	    DSpaceFileManagerFactory.init(properties);
+	}
     }
 
     public static Config getConfig() {
