7bcf5e74c2c2f3fe2e03c10f8d88bcd5bbde2032
Luis Cruz <luis.cruz@ist.utl.pt>
2008-07-06 20:54:56 +0000

    Remove atlassian profiling filter and replacde it with a custome filter that will register all requests, even if exception happen. It's performance will also shave about 2 to 3 ms off every request.

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/servlets/filters/ProfilingFilter.java
@@ -0,0 +1,53 @@
+package pt.ist.fenixWebFramework.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+
+public class ProfilingFilter implements Filter {
+
+    private Logger logger = Logger.getLogger(ProfilingFilter.class);
+
+    @Override
+    public void init(final FilterConfig arg0) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain)
+    		throws IOException, ServletException {
+	final HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+	final String uri = httpServletRequest.getRequestURI();
+
+	final long start = System.currentTimeMillis();
+	try {
+	    filterChain.doFilter(servletRequest, servletResponse);
+	} finally {
+	    final long end = System.currentTimeMillis();
+	    log(uri, start, end);
+	}
+    }
+
+    protected void log(final String uri, final long start, final long end) {
+	final long time = end - start;
+
+	final StringBuilder stringBuilder = new StringBuilder();
+	stringBuilder.append("[");
+	stringBuilder.append(time);
+	stringBuilder.append("mn] - ");
+	stringBuilder.append(uri);
+	logger.info(stringBuilder.toString());
+    }
+
+}
