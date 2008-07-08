a9d161e34f0434536e45b13b57a85b9e29da1db9
Nuno Ochoa <nuno.ochoa@ist.utl.pt>
2008-07-08 17:31:40 +0000

    created email link

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/components/HtmlEMailLink.java
@@ -0,0 +1,12 @@
+package pt.ist.fenixWebFramework.renderers.components;
+
+public class HtmlEMailLink extends HtmlLink {
+
+    public HtmlEMailLink(final String email) {
+	setUrl("mailto:" + email);
+	setText(email);
+	setContextRelative(false);
+	setModuleRelative(false);
+    }
+
+}
