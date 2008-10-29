704e07a17e03a8b7dbe5f621cd136e3f212a58e4
Luis Cruz <luis.cruz@ist.utl.pt>
2008-10-29 11:56:15 +0000

    Filter requests to irrelevant content.

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/servlets/filters/RequestIgnorerFilter.java
@@ -0,0 +1,31 @@
+package pt.ist.fenixWebFramework.servlets.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestIgnorerFilter implements Filter {
+
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain)
+    		throws IOException, ServletException {
+	final HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+	httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+	httpServletResponse.getOutputStream().close();
+    }
+
+}
