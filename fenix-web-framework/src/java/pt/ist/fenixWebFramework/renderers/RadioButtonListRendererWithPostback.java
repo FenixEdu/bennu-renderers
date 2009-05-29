383dc3383d477e15ef3bca0337f6ed3b52127ef9
Luis Cruz <luis.cruz@ist.utl.pt>
2009-05-29 11:46:09 +0000

    New radio select with post-back renderer.

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/RadioButtonListRendererWithPostback.java
@@ -0,0 +1,100 @@
+package pt.ist.fenixWebFramework.renderers;
+
+import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlHiddenField;
+import pt.ist.fenixWebFramework.renderers.components.HtmlInlineContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlRadioButtonList;
+import pt.ist.fenixWebFramework.renderers.components.controllers.HtmlController;
+import pt.ist.fenixWebFramework.renderers.components.state.IViewState;
+import pt.ist.fenixWebFramework.renderers.components.state.ViewDestination;
+import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+import pt.ist.fenixWebFramework.renderers.model.MetaSlot;
+
+/**
+ * This renderer can be used as the input for a list of objects. The list of
+ * objects the user can choose will be presented as an html list were each list
+ * item will contain the presentation of the object and a radio button that
+ * allows to choose that particular object. When submiting, the selected object
+ * will be the value passed to the slot.
+ * 
+ * <p>
+ * Example: <form> <input type="radio" name="option"/>
+ * <em>&lt;object A presentation&gt;</em><br/>
+ * <input type="radio" name="option"/><em>&lt;object B presentation&gt;</em><br/>
+ * <input type="radio" name="option"/><em>&lt;object C presentation&gt;</em>
+ * </form>
+ */
+public class RadioButtonListRendererWithPostback extends RadioButtonListRenderer {
+
+    private final String HIDDEN_NAME = "postback";
+
+    private String destination;
+
+    public String getDestination() {
+	return destination;
+    }
+
+    public void setDestination(String destination) {
+	this.destination = destination;
+    }
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+	final Layout layout = super.getLayout(object, type);
+
+	return layout;
+    }
+
+    class RadioButtonListLayoutWithPostback extends RadioButtonListLayout {
+
+	@Override
+	public HtmlComponent createComponent(Object object, Class type) {
+	    HtmlInlineContainer container = new HtmlInlineContainer();
+
+	    String prefix = ((MetaSlot) getInputContext().getMetaObject()).getName();
+
+	    HtmlHiddenField hidden = new HtmlHiddenField(prefix + HIDDEN_NAME, "");
+
+	    final HtmlRadioButtonList htmlComponent = (HtmlRadioButtonList) super.createComponent(object, type);
+	    htmlComponent.setOnClick("this.form." + prefix + HIDDEN_NAME + ".value='true';this.form.submit();");
+	    htmlComponent.setController(new PostBackController(hidden, destination));
+
+	    container.addChild(hidden);
+	    container.addChild(htmlComponent);
+
+	    return container;
+	}
+    }
+
+    private static class PostBackController extends HtmlController {
+
+	private HtmlHiddenField hidden;
+
+	private String destination;
+
+	public PostBackController(HtmlHiddenField hidden, String destination) {
+	    this.hidden = hidden;
+	    this.destination = destination;
+	}
+
+	@Override
+	public void execute(IViewState viewState) {
+	    if (hidden.getValue() != null && hidden.getValue().equalsIgnoreCase("true")) {
+		String destinationName = this.destination == null ? "postback" : this.destination;
+		ViewDestination destination = viewState.getDestination(destinationName);
+
+		if (destination != null) {
+		    viewState.setCurrentDestination(destination);
+		} else {
+		    viewState.setCurrentDestination("postBack");
+		}
+
+		hidden.setValue("false");
+		viewState.setSkipValidation(true);
+	    }
+
+	}
+
+    }
+
+}
