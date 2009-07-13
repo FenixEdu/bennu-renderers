8263111bdf3cdf67d95edb0a272d1517d7b23073
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-07-13 14:28:23 +0000

    Paragraph container

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/components/HtmlParagraphContainer.java
@@ -0,0 +1,22 @@
+package pt.ist.fenixWebFramework.renderers.components;
+
+import javax.servlet.jsp.PageContext;
+
+import pt.ist.fenixWebFramework.renderers.components.tags.HtmlTag;
+
+public class HtmlParagraphContainer extends HtmlContainer {
+
+    public HtmlParagraphContainer() {
+	super();
+    }
+
+    @Override
+    public HtmlTag getOwnTag(PageContext context) {
+	HtmlTag tag = super.getOwnTag(context);
+
+	tag.setName("p");
+
+	return tag;
+    }
+
+}
