68ee7ff31d595481b19d1a832b1309b21b8fe4c8
Shezad Anavarali <shezad@ist.utl.pt>
2009-09-14 17:38:58 +0000

    initial version

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/servlets/rest/Path.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package pt.ist.fenixWebFramework.servlets.rest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Shezad Anavarali Date: Sep 14, 2009
+ * 
+ */
+@Target( { ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Path {
+
+    String value();
+
+}
---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/servlets/rest/RestAnnotationProcessor.java
@@ -0,0 +1,81 @@
+package pt.ist.fenixWebFramework.servlets.rest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+
+import pt.utl.ist.fenix.tools.util.FileUtils;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedAnnotationTypes( { "pt.ist.fenixWebFramework.servlets.rest.Path" })
+public class RestAnnotationProcessor extends AbstractProcessor {
+
+    static final String LOG_FILENAME = ".restAnnotationLog";
+    public static final String ENTRY_SEPERATOR = "\n";
+    public static final String LINE_SEPERATOR = "\t";
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+	final Map<String, String> resources = new HashMap<String, String>();
+
+	final File file = new File(LOG_FILENAME);
+	if (file.exists()) {
+	    try {
+		final String contents = FileUtils.readFile(LOG_FILENAME);
+		for (final String line : contents.split(ENTRY_SEPERATOR)) {
+		    String[] values = line.split(LINE_SEPERATOR);
+		    if (values.length == 2) {
+			resources.put(values[0], values[1]);
+		    }
+		}
+	    } catch (final IOException e) {
+		e.printStackTrace();
+	    }
+	}
+
+	Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(Path.class);
+	for (final Element element : elementsAnnotatedWith) {
+	    if (element instanceof TypeElement) {
+		resources.put(((TypeElement) element).getQualifiedName().toString(), element.getAnnotation(Path.class).value());
+	    }
+	}
+
+	FileWriter fileWriter = null;
+	try {
+	    fileWriter = new FileWriter(LOG_FILENAME, true);
+	    for (final Entry<String, String> entry : resources.entrySet()) {
+		fileWriter.append(entry.getKey());
+		fileWriter.append(LINE_SEPERATOR);
+		fileWriter.append(entry.getValue());
+		fileWriter.write(ENTRY_SEPERATOR);
+
+	    }
+	} catch (final IOException e) {
+	    e.printStackTrace();
+	} finally {
+	    if (fileWriter != null) {
+		try {
+		    fileWriter.close();
+		} catch (final IOException e) {
+		    e.printStackTrace();
+		}
+	    }
+	}
+
+	return true;
+    }
+
+}
