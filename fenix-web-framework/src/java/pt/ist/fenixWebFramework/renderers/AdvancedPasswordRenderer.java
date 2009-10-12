1c4c523d6aea1d7427bb74fd72b1be28c616cad2
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-10-12 15:49:17 +0000

    Password renderer using the design pattern that passwords should not be entered twice but rather have the option to unmask the password.

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/AdvancedPasswordRenderer.java
@@ -0,0 +1,45 @@
+package pt.ist.fenixWebFramework.renderers;
+
+import pt.ist.fenixWebFramework.renderers.components.HtmlBlockContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlCheckBox;
+import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlPasswordInput;
+import pt.ist.fenixWebFramework.renderers.components.HtmlScript;
+import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+
+public class AdvancedPasswordRenderer extends InputRenderer {
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+	return new Layout() {
+
+	    @Override
+	    public HtmlComponent createComponent(Object object, Class type) {
+		HtmlBlockContainer container = new HtmlBlockContainer();
+		HtmlPasswordInput passwordInput = new HtmlPasswordInput();
+		container.addChild(passwordInput);
+		passwordInput.setId(passwordInput.getName());
+		HtmlCheckBox checkBox = new HtmlCheckBox();
+		checkBox.setText(RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.AdvancedPasswordRenderer.showPassword"));
+		checkBox.setId(checkBox.getName());
+		container.addChild(checkBox);
+		HtmlScript script = new HtmlScript();
+		
+		String passwordInputId = RenderUtils.escapeId(passwordInput.getId());
+		String checkBoxId = RenderUtils.escapeId(checkBox.getId());
+		
+		script.setScript("$(\"#" + checkBoxId +"\").click( function() { " +
+				"var password = $(\"#" + passwordInputId +"\").attr('value');" +
+				"var id = '" + passwordInputId + "';" +
+				"if ($(this).attr('checked')) { $(\"#" + passwordInputId +"\").after(\"<input type='text' id=\" + id + \" value=\" + password + \" />\").remove(); }" +
+				"else { $(\"#" + passwordInputId +"\").after(\"<input type='password' id=\" + id + \" value=\" + password + \" />\").remove(); }" +		
+				"});");
+		container.addChild(script);
+		return container;
+	    }
+	    
+	};
+    }
+
+}
