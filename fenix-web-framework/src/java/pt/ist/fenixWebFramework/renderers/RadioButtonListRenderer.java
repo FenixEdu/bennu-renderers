757f00f1c40dfc63f8c16ee546024f514512bc92
Anil Mamade Ali Kassamali <amak@ist.utl.pt>
2009-10-13 10:21:45 +0000

    Support for Bidirectional converter in radio buttons renderers

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/RadioButtonListRenderer.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/RadioButtonListRenderer.java
@@ -8,6 +8,7 @@ import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
 import pt.ist.fenixWebFramework.renderers.components.HtmlLabel;
 import pt.ist.fenixWebFramework.renderers.components.HtmlRadioButton;
 import pt.ist.fenixWebFramework.renderers.components.HtmlRadioButtonList;
+import pt.ist.fenixWebFramework.renderers.components.converters.BiDirectionalConverter;
 import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
 import pt.ist.fenixWebFramework.renderers.contexts.PresentationContext;
 import pt.ist.fenixWebFramework.renderers.layouts.Layout;
@@ -22,17 +23,16 @@ import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
 
 /**
  * This renderer can be used as the input for a list of objects. The list of
- * objects the user can choose will be presented as an html list were each
- * list item will contain the presentation of the object and a radio button that
+ * objects the user can choose will be presented as an html list were each list
+ * item will contain the presentation of the object and a radio button that
  * allows to choose that particular object. When submiting, the selected object
  * will be the value passed to the slot.
  * 
- * <p> 
- * Example:
- * <form>
- *      <input type="radio" name="option"/><em>&lt;object A presentation&gt;</em><br/>
- *      <input type="radio" name="option"/><em>&lt;object B presentation&gt;</em><br/>
- *      <input type="radio" name="option"/><em>&lt;object C presentation&gt;</em>
+ * <p>
+ * Example: <form> <input type="radio" name="option"/>
+ * <em>&lt;object A presentation&gt;</em><br/>
+ * <input type="radio" name="option"/><em>&lt;object B presentation&gt;</em><br/>
+ * <input type="radio" name="option"/><em>&lt;object C presentation&gt;</em>
  * </form>
  */
 public class RadioButtonListRenderer extends InputRenderer {
@@ -49,9 +49,9 @@ public class RadioButtonListRenderer extends InputRenderer {
     private DataProvider provider;
 
     private String sortBy;
-    
+
     private boolean saveOptions;
-    
+
     /**
      * This property allows you to configure the class attribute for each
      * object's presentation.
@@ -59,11 +59,11 @@ public class RadioButtonListRenderer extends InputRenderer {
      * @property
      */
     public void setEachClasses(String classes) {
-        this.eachClasses = classes;
+	this.eachClasses = classes;
     }
 
     public String getEachClasses() {
-        return this.eachClasses;
+	return this.eachClasses;
     }
 
     /**
@@ -73,15 +73,15 @@ public class RadioButtonListRenderer extends InputRenderer {
      * @property
      */
     public void setEachStyle(String style) {
-        this.eachStyle = style;
+	this.eachStyle = style;
     }
 
     public String getEachStyle() {
-        return this.eachStyle;
+	return this.eachStyle;
     }
 
     public String getEachLayout() {
-        return eachLayout;
+	return eachLayout;
     }
 
     /**
@@ -90,33 +90,33 @@ public class RadioButtonListRenderer extends InputRenderer {
      * @property
      */
     public void setEachLayout(String eachLayout) {
-        this.eachLayout = eachLayout;
+	this.eachLayout = eachLayout;
     }
 
     public String getEachSchema() {
-        return eachSchema;
+	return eachSchema;
     }
 
     /**
-     * Allows you to specify the schema that should be used when presenting
-     * each individual object.
+     * Allows you to specify the schema that should be used when presenting each
+     * individual object.
      * 
      * @property
      */
     public void setEachSchema(String eachSchema) {
-        this.eachSchema = eachSchema;
+	this.eachSchema = eachSchema;
     }
 
     public String getProviderClass() {
-        return this.providerClass;
+	return this.providerClass;
     }
 
     /**
-     * The class name of a {@link DataProvider data provider}. The data
-     * provider is responsible for providing a collection of objects. This
-     * collection represents all the possible options for the slot beeing
-     * edited. Additionally the data provider can also provide a custom
-     * converter for the object encoded values.
+     * The class name of a {@link DataProvider data provider}. The data provider
+     * is responsible for providing a collection of objects. This collection
+     * represents all the possible options for the slot beeing edited.
+     * Additionally the data provider can also provide a custom converter for
+     * the object encoded values.
      * 
      * <p>
      * If the object in the slot is part of the collections of objects it will
@@ -125,11 +125,11 @@ public class RadioButtonListRenderer extends InputRenderer {
      * @property
      */
     public void setProviderClass(String providerClass) {
-        this.providerClass = providerClass;
+	this.providerClass = providerClass;
     }
-    
+
     public String getSortBy() {
-        return this.sortBy;
+	return this.sortBy;
     }
 
     /**
@@ -140,172 +140,172 @@ public class RadioButtonListRenderer extends InputRenderer {
      * @property
      */
     public void setSortBy(String sortBy) {
-        this.sortBy = sortBy;
+	this.sortBy = sortBy;
     }
-    
+
     public boolean isSaveOptions() {
-		return saveOptions;
-	}
+	return saveOptions;
+    }
 
     /**
-     * Allows the possible object list to be persisted between requests,
-     * meaning that the provider is invoked only once.
+     * Allows the possible object list to be persisted between requests, meaning
+     * that the provider is invoked only once.
      * 
      * @property
      */
-	public void setSaveOptions(boolean saveOptions) {
-		this.saveOptions = saveOptions;
-	}
+    public void setSaveOptions(boolean saveOptions) {
+	this.saveOptions = saveOptions;
+    }
 
-	@Override
+    @Override
     protected Layout getLayout(Object object, Class type) {
-        return new RadioButtonListLayout();
+	return new RadioButtonListLayout();
     }
 
     protected DataProvider getProvider() {
-        if (this.provider == null) {
-            String className = getProviderClass();
-            
-            try {
-                Class providerCass = (Class<DataProvider>) Class.forName(className);
-                this.provider = (DataProvider) providerCass.newInstance();  
-            } catch (Exception e) {
-                throw new RuntimeException("could not get a data provider instance", e);
-            }
-        }
-        
-        return this.provider;
+	if (this.provider == null) {
+	    String className = getProviderClass();
+
+	    try {
+		Class providerCass = (Class<DataProvider>) Class.forName(className);
+		this.provider = (DataProvider) providerCass.newInstance();
+	    } catch (Exception e) {
+		throw new RuntimeException("could not get a data provider instance", e);
+	    }
+	}
+
+	return this.provider;
     }
-    
+
     protected Converter getConverter() {
-        DataProvider provider = getProvider();
-        
-        return provider.getConverter();
+	DataProvider provider = getProvider();
+
+	return provider.getConverter();
     }
 
     protected Collection getPossibleObjects() {
-        Object object = getInputContext().getParentContext().getMetaObject().getObject();
-        Object value = getInputContext().getMetaObject().getObject();
+	Object object = getInputContext().getParentContext().getMetaObject().getObject();
+	Object value = getInputContext().getMetaObject().getObject();
 
-        if (getProviderClass() != null) {
-            try {
-                DataProvider provider = getProvider();
-                Collection collection = (Collection) provider.provide(object, value);
-                
-                if (getSortBy() == null) {
-                    return collection;
-                }
-                else {
-                    return RenderUtils.sortCollectionWithCriteria(collection, getSortBy());
-                }
-            }
-            catch (Exception e) {
-                throw new RuntimeException("exception while executing data provider", e);
-            }
-        }
-        else {
-            throw new RuntimeException("a data provider must be supplied");
-        }
+	if (getProviderClass() != null) {
+	    try {
+		DataProvider provider = getProvider();
+		Collection collection = (Collection) provider.provide(object, value);
+
+		if (getSortBy() == null) {
+		    return collection;
+		} else {
+		    return RenderUtils.sortCollectionWithCriteria(collection, getSortBy());
+		}
+	    } catch (Exception e) {
+		throw new RuntimeException("exception while executing data provider", e);
+	    }
+	} else {
+	    throw new RuntimeException("a data provider must be supplied");
+	}
     }
-    
+
     class RadioButtonListLayout extends Layout {
-        
-        @Override
-        public HtmlComponent createComponent(Object object, Class type) {
-            HtmlRadioButtonList listComponent = new HtmlRadioButtonList();
-            
-            Schema schema = RenderKit.getInstance().findSchema(getEachSchema());
-            
-            List<MetaObject> possibleMetaObjects;
 
-            if (hasSavedPossibleMetaObjects()) {
-                possibleMetaObjects = getPossibleMetaObjects();
-            } else {
-                possibleMetaObjects = new ArrayList<MetaObject>();
-                
-                for (Object possibility : getPossibleObjects()) {
-                    possibleMetaObjects.add(MetaObjectFactory.createObject(possibility, schema));
-                }
-            }
-            
-            for (MetaObject metaObject : possibleMetaObjects) {
-                Object obj = metaObject.getObject();
-                MetaObjectKey key = metaObject.getKey();
-                
-                String layout = getEachLayout();
-                
-                PresentationContext newContext = getContext().createSubContext(metaObject);
-                newContext.setLayout(layout);
-                newContext.setRenderMode(RenderMode.getMode("output"));
-                
-                RenderKit kit = RenderKit.getInstance();
-                HtmlComponent component = kit.render(newContext, obj);
-                
-                HtmlLabel label = new HtmlLabel();
-                label.setBody(component);
-                label.setStyle(eachStyle);
-                label.setClasses(eachClasses);
-                
-                HtmlRadioButton radioButton = listComponent.addOption(label, key.toString());
-                label.setFor(radioButton);
-                
-                if (object != null && object.equals(obj)) {
-                    radioButton.setChecked(true);
-                }
-            }
+	@Override
+	public HtmlComponent createComponent(Object object, Class type) {
+	    HtmlRadioButtonList listComponent = new HtmlRadioButtonList();
 
-            if (isSaveOptions()) {
-            	    savePossibleMetaObjects(possibleMetaObjects);
-            }
-            
-            listComponent.setConverter(new OptionConverter(possibleMetaObjects, getConverter()));
-            listComponent.setTargetSlot((MetaSlotKey) getInputContext().getMetaObject().getKey());
-            
-            return listComponent;
-        }
+	    Schema schema = RenderKit.getInstance().findSchema(getEachSchema());
 
-		private boolean hasSavedPossibleMetaObjects() {
-			return getInputContext().getViewState().getLocalAttribute("options") != null;
+	    List<MetaObject> possibleMetaObjects;
+
+	    if (hasSavedPossibleMetaObjects()) {
+		possibleMetaObjects = getPossibleMetaObjects();
+	    } else {
+		possibleMetaObjects = new ArrayList<MetaObject>();
+
+		for (Object possibility : getPossibleObjects()) {
+		    possibleMetaObjects.add(MetaObjectFactory.createObject(possibility, schema));
 		}
+	    }
 
-		private List<MetaObject> getPossibleMetaObjects() {
-			return (List<MetaObject>) getInputContext().getViewState().getLocalAttribute("options");
+	    for (MetaObject metaObject : possibleMetaObjects) {
+		Object obj = metaObject.getObject();
+		MetaObjectKey key = metaObject.getKey();
+
+		String layout = getEachLayout();
+
+		PresentationContext newContext = getContext().createSubContext(metaObject);
+		newContext.setLayout(layout);
+		newContext.setRenderMode(RenderMode.getMode("output"));
+
+		RenderKit kit = RenderKit.getInstance();
+		HtmlComponent component = kit.render(newContext, obj);
+
+		HtmlLabel label = new HtmlLabel();
+		label.setBody(component);
+		label.setStyle(eachStyle);
+		label.setClasses(eachClasses);
+
+		String optionValue = getConverter() instanceof BiDirectionalConverter ? ((BiDirectionalConverter) getConverter())
+			.deserialize(obj) : key.toString();
+
+		HtmlRadioButton radioButton = listComponent.addOption(label, optionValue);
+		label.setFor(radioButton);
+
+		if (object != null && object.equals(obj)) {
+		    radioButton.setChecked(true);
 		}
-		
-		private void savePossibleMetaObjects(List<MetaObject> possibleMetaObjects) {
-			getInputContext().getViewState().setLocalAttribute("options", possibleMetaObjects);
-		}
-        
+	    }
+
+	    if (isSaveOptions()) {
+		savePossibleMetaObjects(possibleMetaObjects);
+	    }
+
+	    listComponent.setConverter(new OptionConverter(possibleMetaObjects, getConverter()));
+	    listComponent.setTargetSlot((MetaSlotKey) getInputContext().getMetaObject().getKey());
+
+	    return listComponent;
+	}
+
+	private boolean hasSavedPossibleMetaObjects() {
+	    return getInputContext().getViewState().getLocalAttribute("options") != null;
+	}
+
+	private List<MetaObject> getPossibleMetaObjects() {
+	    return (List<MetaObject>) getInputContext().getViewState().getLocalAttribute("options");
+	}
+
+	private void savePossibleMetaObjects(List<MetaObject> possibleMetaObjects) {
+	    getInputContext().getViewState().setLocalAttribute("options", possibleMetaObjects);
+	}
+
     }
-    
+
     private static class OptionConverter extends Converter {
 
-        private List<MetaObject> metaObjects;
-        private Converter converter;
+	private List<MetaObject> metaObjects;
+	private Converter converter;
 
-        public OptionConverter(List<MetaObject> metaObjects, Converter converter) {
-            this.metaObjects = metaObjects;
-            this.converter = converter;
-        }
+	public OptionConverter(List<MetaObject> metaObjects, Converter converter) {
+	    this.metaObjects = metaObjects;
+	    this.converter = converter;
+	}
 
-        @Override
-        public Object convert(Class type, Object value) {
-            String textValue = (String) value;
+	@Override
+	public Object convert(Class type, Object value) {
+	    String textValue = (String) value;
 
-            if (textValue == null || textValue.length() == 0) {
-                return null;
-            } else {
-                if (this.converter != null) {
-                    return this.converter.convert(type, value);
-                } else {
-                    for (MetaObject metaObject : this.metaObjects) {
-                        if (textValue.equals(metaObject.getKey().toString())) {
-                            return metaObject.getObject();
-                        }
-                    }
-                    return null;
-                }
-            }
-        }
+	    if (textValue == null || textValue.length() == 0) {
+		return null;
+	    } else {
+		if (this.converter != null) {
+		    return this.converter.convert(type, value);
+		} else {
+		    for (MetaObject metaObject : this.metaObjects) {
+			if (textValue.equals(metaObject.getKey().toString())) {
+			    return metaObject.getObject();
+			}
+		    }
+		    return null;
+		}
+	    }
+	}
     }
 }
