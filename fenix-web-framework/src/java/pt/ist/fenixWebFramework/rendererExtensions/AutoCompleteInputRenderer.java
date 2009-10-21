03253227f5fd324b44d983f79921c273e712a229
Paulo Abrantes <paulo.abrantes@ist.utl.pt>
2009-10-21 17:18:15 +0000

    AutoComplete script now also deals with server side errors that are sent to the client, allowing the client to know that something went wrong with the search running on the server

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/AutoCompleteInputRenderer.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/AutoCompleteInputRenderer.java
@@ -374,7 +374,7 @@ public class AutoCompleteInputRenderer extends InputRenderer {
 		String finalUri = link.calculateUrl();
 		String scriptText = "$(\"input#" + escapeId(textFieldId) + "\").autocomplete(\"" + finalUri + "\", { minChars: "
 			+ getMinChars() + ", validSelection: false"
-			+ ", cleanSelection: clearAutoComplete, select: selectElement, after: updateCustomValue});";
+			+ ", cleanSelection: clearAutoComplete, select: selectElement, after: updateCustomValue, error:showError});";
 
 		HtmlScript script = new HtmlScript();
 		script.setContentType("text/javascript");
