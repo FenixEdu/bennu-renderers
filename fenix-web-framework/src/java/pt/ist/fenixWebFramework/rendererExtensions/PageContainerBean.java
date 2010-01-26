ee62414330739d4524ba8c2c01cca55720e433b6
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2010-01-26 16:00:23 +0000

    DomainReference removed

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/PageContainerBean.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/PageContainerBean.java
@@ -5,30 +5,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import pt.ist.fenixWebFramework.util.DomainListReference;
-import pt.ist.fenixWebFramework.util.DomainReference;
 import pt.ist.fenixframework.DomainObject;
 
 public class PageContainerBean implements Serializable {
-    
+
     private transient List<? extends DomainObject> objects;
-    private DomainListReference<DomainObject> pageObjects;
-    private DomainReference<DomainObject> selected;
+    private List<DomainObject> pageObjects;
+    private DomainObject selected;
     private Integer numberOfPages;
-    
+
     private Integer page = 1;
-    
+
     public List<? extends DomainObject> getObjects() {
-        return objects;
+	return objects;
     }
 
     public void setObjects(List<? extends DomainObject> objects) {
 	this.objects = objects;
-        setPageObjects(null);
+	setPageObjects(null);
     }
-    
+
     public List<DomainObject> getPageObjects() {
-	if(this.pageObjects != null) {
+	if (this.pageObjects != null) {
 	    List<DomainObject> result = new ArrayList<DomainObject>();
 	    for (DomainObject domainObject : this.pageObjects) {
 		result.add(domainObject);
@@ -40,8 +38,8 @@ public class PageContainerBean implements Serializable {
     }
 
     public void setPageObjects(List<? extends DomainObject> pageObjects) {
-	if(pageObjects != null) {
-	    this.pageObjects = new DomainListReference<DomainObject>();
+	if (pageObjects != null) {
+	    this.pageObjects = new ArrayList<DomainObject>();
 	    for (DomainObject object : pageObjects) {
 		this.pageObjects.add(object);
 	    }
@@ -51,28 +49,28 @@ public class PageContainerBean implements Serializable {
     }
 
     public Integer getPage() {
-        return page;
+	return page;
     }
 
     public void setPage(Integer page) {
-        this.page = page;
+	this.page = page;
     }
-    
+
     public DomainObject getSelected() {
-	return (this.selected != null) ? this.selected.getObject() : null;
+	return selected;
     }
 
     public void setSelected(DomainObject selected) {
-	this.selected = (selected != null) ? new DomainReference<DomainObject>(selected) : null;
+	this.selected = selected;
     }
-    
-    public List<DomainObject> getPageByPageSize(int pageSize){
-	if(getPageObjects() != null) {
+
+    public List<DomainObject> getPageByPageSize(int pageSize) {
+	if (getPageObjects() != null) {
 	    return getPageObjects();
 	} else {
-	    if(getObjects() != null && !getObjects().isEmpty()) {
+	    if (getObjects() != null && !getObjects().isEmpty()) {
 		validatePageNumber(pageSize);
-		int from = (getPage() - 1)  * pageSize;
+		int from = (getPage() - 1) * pageSize;
 		int to = getObjects().size() > getPage() * pageSize ? getPage() * pageSize : getObjects().size();
 		List<DomainObject> subList = new ArrayList<DomainObject>(getObjects().subList(from, to));
 		setPageObjects(subList);
@@ -82,51 +80,51 @@ public class PageContainerBean implements Serializable {
 	    }
 	}
     }
-    
+
     private void validatePageNumber(int pageSize) {
-	if(getPage() < 1) {
+	if (getPage() < 1) {
 	    setPage(1);
 	} else {
 	    Integer numberOfPages = getNumberOfPages(pageSize);
-	    if(getPage() > numberOfPages) {
+	    if (getPage() > numberOfPages) {
 		setPage(numberOfPages);
 	    }
 	}
-	
+
     }
 
     public int getNumberOfPages(int pageSize) {
-	if(getObjects() != null) {
+	if (getObjects() != null) {
 	    this.numberOfPages = (int) Math.ceil((double) getObjects().size() / pageSize);
 	}
 	return this.numberOfPages;
     }
-    
+
     public boolean hasNextPage(int pageSize) {
 	return getPage() < getNumberOfPages(pageSize);
     }
-    
+
     public boolean hasPreviousPage(int pageSize) {
-	return getPage() > 1; 
+	return getPage() > 1;
     }
 
     public List<? extends DomainObject> getAllObjects() {
-	if(getPageObjects() != null) {
+	if (getPageObjects() != null) {
 	    return getPageObjects();
 	} else {
-	    if(getObjects() != null) {
+	    if (getObjects() != null) {
 		setPageObjects(getObjects());
 		return getObjects();
 	    } else {
 		return Collections.emptyList();
 	    }
-	} 
+	}
     }
-    
+
     public void setPageJump(Integer pageJump) {
 	setPage(pageJump);
     }
-    
+
     public Integer getPageJump() {
 	return null;
     }
