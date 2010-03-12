69c9c9c846d660dadf94b6582b8841608fd00b6f
Daniel Ribeiro <daniel.v.ribeiro@ist.utl.pt>
2010-03-12 10:31:00 +0000

    New: fenix-render-ISBNValidator It was included in a package with source code for org\apache\commons\validator\routines, as the version that includes this package is not stable (apache-validators 1.4-SNAPSHOT)

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/validators/ISBNValidator.java
@@ -0,0 +1,20 @@
+package pt.ist.fenixWebFramework.renderers.validators;
+
+public class ISBNValidator extends HtmlValidator {
+
+    public ISBNValidator() {
+	setMessage("renderers.validator.isbn.error");
+    }
+
+    public ISBNValidator(HtmlChainValidator htmlChainValidator) {
+	super(htmlChainValidator);
+	setMessage("renderers.validator.isbn.error");
+    }
+
+    @Override
+    public void performValidation() {
+	boolean valid = new org.apache.commons.validator.routines.ISBNValidator().isValid(getComponent().getValue());
+	setValid(valid);
+
+    }
+}
