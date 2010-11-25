032285521482802020ccc25a6108057299333e6d
João Antunes <joao.antunes@tagus.ist.utl.pt>
2010-11-25 19:17:07 +0000

    New Feature: added the RequireNrItemsValidator tested and working for the net.sourceforge.fenixedu.presentationTier.renderers.InputMenuOptionListRendererWithPostback associated with the 'menu-select-postback' layout, but that should work with all of the renders that post a list of checkboxes

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/validators/RequiredNrItemsValidator.java
@@ -0,0 +1,50 @@
+package pt.ist.fenixWebFramework.renderers.validators;
+
+import pt.ist.fenixWebFramework.renderers.components.HtmlCheckBoxList;
+import pt.ist.fenixWebFramework.renderers.components.HtmlFormComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlScript;
+import pt.ist.fenixWebFramework.renderers.components.HtmlSimpleValueComponent;
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+
+public class RequiredNrItemsValidator extends HtmlValidator {
+    private Integer nrRequiredItems;
+
+    public RequiredNrItemsValidator() {
+	super();
+	setMessage("renderers.validator.invalid.nrItems");
+    }
+
+    public RequiredNrItemsValidator(HtmlChainValidator htmlChainValidator) {
+	super(htmlChainValidator);
+
+	setMessage("renderers.validator.invalid.nrItems");
+    }
+
+    @Override
+    public void performValidation() {
+	HtmlCheckBoxList component = (HtmlCheckBoxList) getComponent();
+
+	String values[] = component.getValues();
+	if (getNrRequiredItems() == null)
+	{
+	    throw new RuntimeException("renderers.validator.nr.items.not.specified");
+	}
+	    
+
+	setValid(values.length >= getNrRequiredItems().intValue());
+
+    }
+
+    public void setNrRequiredItems(Integer nrRequiredItems) {
+	String []arguments = new String[1];
+	arguments[0] = nrRequiredItems.toString();
+	setArguments(arguments);
+	this.nrRequiredItems = nrRequiredItems;
+    }
+
+    public Integer getNrRequiredItems() {
+	return nrRequiredItems;
+    }
+
+
+}
