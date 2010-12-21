c8093e023751e0cc5ed8b73b716bcec5d32c4b43
Susana Fernandes <susana.fernandes@ist.utl.pt>
2010-12-21 11:28:08 +0000

    Minor Change: User RenderUtils.getEnumString(enum) to get enum description.

--- fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/EnumSetRenderer.java
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/EnumSetRenderer.java
@@ -8,54 +8,52 @@ import pt.ist.fenixWebFramework.renderers.layouts.Layout;
 import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
 
 /**
- * The <code>EnumSetRenderer</code> provides a simple presentation for enumeration set values. An
- * enumSet value will be displayed in one of two forms. First a bundle named
- * <code>ENUMERATION_RESOURCES</code> is used. Using as key the enum name a localized message is
- * searched. If the bundle is not defined or the key does not exist in the bundle then the programmatic
- * name of the enum is presented.
+ * The <code>EnumSetRenderer</code> provides a simple presentation for
+ * enumeration set values. An enumSet value will be displayed in one of two
+ * forms. First a bundle named <code>ENUMERATION_RESOURCES</code> is used. Using
+ * as key the enum name a localized message is searched. If the bundle is not
+ * defined or the key does not exist in the bundle then the programmatic name of
+ * the enum is presented.
  * 
  */
 public class EnumSetRenderer extends OutputRenderer {
 
-    // NOTE: duplicate code with EnumInputRenderer
-    protected String getEnumSetDescription(EnumSet enumset) {
+	// NOTE: duplicate code with EnumInputRenderer
+	protected String getEnumSetDescription(EnumSet enumset) {
 
-        Object[] enumSetArray = enumset.toArray();
-        StringBuilder description = new StringBuilder();
+		Object[] enumSetArray = enumset.toArray();
+		StringBuilder description = new StringBuilder();
 
-        for (Object enumSetObject : enumSetArray) {
+		for (Object enumSetObject : enumSetArray) {
 
-            String thisDescription = RenderUtils.getResourceString("ENUMERATION_RESOURCES",
-                    enumSetObject.toString());
-            if (thisDescription == null) {
-                thisDescription = RenderUtils.getResourceString(enumSetObject.toString());
-            }
+			String thisDescription = RenderUtils
+					.getEnumString((Enum) enumSetObject);
 
-            if (description.length() != 0) {
-                description.append(", ");
-            }
-            description.append(thisDescription);
-        }
-        return description.toString();
-    }
+			if (description.length() != 0) {
+				description.append(", ");
+			}
+			description.append(thisDescription);
+		}
+		return description.toString();
+	}
 
-    @Override
-    protected Layout getLayout(Object object, Class type) {
-        return new Layout() {
+	@Override
+	protected Layout getLayout(Object object, Class type) {
+		return new Layout() {
 
-            @Override
-            public HtmlComponent createComponent(Object object, Class type) {
-                EnumSet enumSet = (EnumSet) object;
+			@Override
+			public HtmlComponent createComponent(Object object, Class type) {
+				EnumSet enumSet = (EnumSet) object;
 
-                if (enumSet == null) {
-                    return new HtmlText();
-                }
+				if (enumSet == null) {
+					return new HtmlText();
+				}
 
-                String description = getEnumSetDescription(enumSet);
+				String description = getEnumSetDescription(enumSet);
 
-                return new HtmlText(description);
-            }
+				return new HtmlText(description);
+			}
 
-        };
-    }
+		};
+	}
 }
