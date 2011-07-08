0f3d539e1076b050ce078cd624a91236ca82f50e
Pedro Amaral <pedro.m.t.amaral@ist.utl.pt>
2011-07-08 15:52:34 +0000

    newFeature: LocalDateValidator usefull to some JobBank forms

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/validators/LocalDateValidator.java
@@ -0,0 +1,78 @@
+package pt.ist.fenixWebFramework.rendererExtensions.validators;
+
+import org.joda.time.LocalDate;
+
+import pt.ist.fenixWebFramework.rendererExtensions.DateTimeInputRenderer.DateTimeConverter;
+import pt.ist.fenixWebFramework.renderers.components.HtmlSimpleValueComponent;
+import pt.ist.fenixWebFramework.renderers.validators.HtmlChainValidator;
+import pt.ist.fenixWebFramework.renderers.validators.HtmlValidator;
+
+public class LocalDateValidator extends HtmlValidator {
+
+    private boolean required;
+
+    public LocalDateValidator() {
+	super();
+	setKey(true);
+    }
+
+    public LocalDateValidator(HtmlChainValidator htmlChainValidator) {
+	super(htmlChainValidator);
+	setKey(true);
+    }
+
+    public boolean isRequired() {
+	return this.required;
+    }
+
+    public void setRequired(boolean required) {
+	this.required = required;
+    }
+
+    @Override
+    public void performValidation() {
+	HtmlSimpleValueComponent component = (HtmlSimpleValueComponent) getComponent();
+
+	String value = component.getValue();
+
+	if (value == null || value.length() == 0) {
+	    setMessage("renderers.validator.dateTime.required");
+	    setValid(!isRequired());
+	} else {
+	    if (value.equals(DateTimeConverter.INVALID)) {
+		setMessage("renderers.validator.dateTime.invalid");
+		setValid(false);
+	    } else if (value.equals(DateTimeConverter.INCOMPLETE)) {
+		setMessage("renderers.validator.dateTime.incomplete");
+		setValid(false);
+	    } else {
+		String[] dateParts = value.split("/");
+
+		if (dateParts.length == 3) {
+		    try {
+
+			int day = Integer.valueOf(dateParts[0]);
+			int month = Integer.valueOf(dateParts[1]);
+			int year = Integer.valueOf(dateParts[2]);
+			LocalDate test = new LocalDate(year, month, day);
+
+		    } catch (NumberFormatException e) {
+			setMessage("renderers.validator.dateTime.notNumbers");
+			setValid(false);
+		    } catch (IllegalArgumentException e) {
+			setMessage("renderers.validator.dateTime.invalid");
+			setValid(false);
+		    }
+		} else {
+		    setMessage("renderers.validator.dateTime.invalid");
+		    setValid(false);
+		}
+	    }
+	}
+    }
+
+    private boolean inRange(int value, int min, int max) {
+	return value >= min && value <= max;
+    }
+
+}
