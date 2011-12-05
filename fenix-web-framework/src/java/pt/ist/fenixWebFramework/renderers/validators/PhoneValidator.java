b717f560475320c879a4a43724d77c0a367c5cce
Sérgio Silva <sergio.silva@ist.utl.pt>
2011-12-05 15:19:13 +0000

    New Feature: add phone validator

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/renderers/validators/PhoneValidator.java
@@ -0,0 +1,26 @@
+package pt.ist.fenixWebFramework.renderers.validators;
+
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+import pt.utl.ist.fenix.tools.util.PhoneUtil;
+
+public class PhoneValidator extends HtmlValidator {
+
+    public PhoneValidator() {
+	super();
+    }
+
+    public PhoneValidator(HtmlChainValidator htmlChainValidator) {
+	super(htmlChainValidator);
+    }
+
+    @Override
+    public void performValidation() {
+	setValid(PhoneUtil.isValidNumber(getComponent().getValue()));
+    }
+
+    @Override
+    public String getErrorMessage() {
+	return RenderUtils.getResourceString("renderers.validator.phone.number");
+    }
+
+}
