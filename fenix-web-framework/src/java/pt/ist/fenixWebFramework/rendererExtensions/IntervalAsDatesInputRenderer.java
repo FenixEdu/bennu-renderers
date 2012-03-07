e307de6b23d0e70e4cf85055b24f036255093cdf
João Carvalho <joao.pedro.carvalho@ist.utl.pt>
2012-03-07 17:43:27 +0000

    New Feature: Added a renderer for the Joda type Interval, which represents the Interval as a start and end date.

---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/IntervalAsDatesInputRenderer.java
@@ -0,0 +1,259 @@
+/**
+ * 
+ */
+package pt.ist.fenixWebFramework.rendererExtensions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.util.RequestUtils;
+import org.joda.time.Interval;
+
+import pt.ist.fenixWebFramework.rendererExtensions.DateTimeInputRenderer.DateTimeConverter;
+import pt.ist.fenixWebFramework.renderers.InputRenderer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlHiddenField;
+import pt.ist.fenixWebFramework.renderers.components.HtmlInlineContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlParagraphContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlText;
+import pt.ist.fenixWebFramework.renderers.components.HtmlTextInput;
+import pt.ist.fenixWebFramework.renderers.components.controllers.HtmlController;
+import pt.ist.fenixWebFramework.renderers.components.converters.ConversionException;
+import pt.ist.fenixWebFramework.renderers.components.converters.Converter;
+import pt.ist.fenixWebFramework.renderers.components.state.IViewState;
+import pt.ist.fenixWebFramework.renderers.converters.DateConverter;
+import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+import pt.ist.fenixWebFramework.renderers.model.MetaSlotKey;
+
+/**
+ * Input Renderer for the Joda Interval class.
+ * 
+ * @author Joao Carvalho (joao.pedro.carvalho@ist.utl.pt)
+ * 
+ */
+@SuppressWarnings("rawtypes")
+public class IntervalAsDatesInputRenderer extends InputRenderer {
+
+    private String startLabel;
+    private String endLabel;
+
+    private boolean multiLine = true;
+
+    public boolean isMultiLine() {
+	return multiLine;
+    }
+
+    public void setMultiLine(boolean multiLine) {
+	this.multiLine = multiLine;
+    }
+
+    public String getStartLabel() {
+	return startLabel;
+    }
+
+    public void setStartLabel(String startLabel) {
+	this.startLabel = startLabel;
+    }
+
+    public String getEndLabel() {
+	return endLabel;
+    }
+
+    public void setEndLabel(String endLabel) {
+	this.endLabel = endLabel;
+    }
+
+    // Format in which the date will be introduced/presented
+    private String dateFormat;
+
+    // Maximum size for the introduced date
+    private String dateSize;
+    private Integer dateMaxLength = 10;
+
+    public String getDateFormat() {
+	return this.dateFormat == null ? DateConverter.DEFAULT_FORMAT : dateFormat;
+    }
+
+    public void setDateFormat(String dateFormat) {
+	this.dateFormat = dateFormat;
+    }
+
+    public Integer getDateMaxLength() {
+	return this.dateMaxLength;
+    }
+
+    public void setDateMaxLength(Integer dateMaxLength) {
+	this.dateMaxLength = dateMaxLength;
+    }
+
+    public String getDateSize() {
+	return this.dateSize;
+    }
+
+    public void setDateSize(String dateSize) {
+	this.dateSize = dateSize;
+    }
+
+    protected Locale getLocale() {
+	HttpServletRequest request = getInputContext().getViewState().getRequest();
+	return RequestUtils.getUserLocale(request, null);
+    }
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+	return new IntervalLayout(new SimpleDateFormat(getDateFormat(), getLocale()), startLabel, endLabel);
+    }
+
+    /*
+     * Layout for the renderer
+     */
+
+    public class IntervalLayout extends Layout {
+	private final DateFormat dateFormat;
+	private final String startLabel, endLabel;
+	private final boolean multiLine = isMultiLine();
+
+	public IntervalLayout(DateFormat simpleDateFormat, String startLabel, String endLabel) {
+	    this.dateFormat = simpleDateFormat;
+	    this.startLabel = startLabel;
+	    this.endLabel = endLabel;
+	}
+
+	private HtmlContainer getContainer() {
+	    return multiLine ? new HtmlParagraphContainer() : new HtmlInlineContainer();
+	}
+
+	@Override
+	public HtmlComponent createComponent(Object object, Class type) {
+	    Interval interval = (Interval) object;
+
+	    MetaSlotKey key = (MetaSlotKey) getInputContext().getMetaObject().getKey();
+
+	    HtmlContainer parentContainer = new HtmlInlineContainer();
+
+	    // Hidden field that will contain the intermediate data between the
+	    // Controller and the Converter
+	    HtmlHiddenField hiddenField = new HtmlHiddenField();
+	    hiddenField.setTargetSlot(key);
+	    parentContainer.addChild(hiddenField);
+
+	    HtmlContainer startContainer = getContainer();
+
+	    if (this.startLabel != null) {
+		startContainer.addChild(new HtmlText(this.startLabel));
+	    }
+
+	    HtmlTextInput startDateField = new HtmlTextInput();
+	    startDateField.setName(key.toString() + "_StartDate");
+	    startDateField.setSize(getDateSize());
+	    startDateField.setMaxLength(getDateMaxLength());
+	    startContainer.addChild(startDateField);
+
+	    startContainer.addChild(new HtmlText(getFormatLabel()));
+
+	    HtmlContainer endContainer = getContainer();
+
+	    if (this.endLabel != null) {
+		endContainer.addChild(new HtmlText(this.endLabel));
+	    }
+
+	    HtmlTextInput endDateField = new HtmlTextInput();
+	    endDateField.setName(key.toString() + "_EndDate");
+	    endDateField.setSize(getDateSize());
+	    endDateField.setMaxLength(getDateMaxLength());
+	    endContainer.addChild(endDateField);
+
+	    endContainer.addChild(new HtmlText(getFormatLabel()));
+
+	    parentContainer.addChild(startContainer);
+
+	    parentContainer.addChild(endContainer);
+
+	    if (interval != null) {
+		startDateField.setValue(dateFormat.format(interval.getStart().toDate()));
+		endDateField.setValue(dateFormat.format(interval.getEnd().toDate()));
+	    }
+
+	    hiddenField.setConverter(new IntervalConverter());
+	    endDateField.setController(new IntervalController(hiddenField, dateFormat, startDateField, endDateField));
+
+	    return parentContainer;
+	}
+
+	protected String getFormatLabel() {
+	    return getDateFormat();
+	}
+    }
+
+    /*
+     * Takes the string representing the interval, and creates the corresponding
+     * interval. The format is as defined in IntervalController
+     */
+
+    @SuppressWarnings("serial")
+    public static class IntervalConverter extends Converter {
+
+	public static final String INVALID = "invalid";
+
+	@Override
+	public Interval convert(Class type, Object value) {
+	    if (value == null || String.valueOf(value).length() == 0) {
+		return null;
+	    }
+	    try {
+		if (value.equals(INVALID))
+		    throw new ConversionException("fenix.renderers.converter.interval.convert", true, value);
+
+		String[] fields = String.valueOf(value).split(";");
+
+		return new Interval(Long.parseLong(fields[0]), Long.parseLong(fields[1]));
+	    } catch (Exception e) {
+		throw new ConversionException("fenix.renderers.converter.interval.convert", e, true, value);
+	    }
+	}
+    }
+
+    /*
+     * Takes the start and end date, and puts the data in the hidden field, in
+     * the format {startMillis};{endMillis}
+     */
+
+    @SuppressWarnings("serial")
+    private class IntervalController extends HtmlController {
+
+	private final HtmlHiddenField valueField;
+	private final DateFormat dateFormat;
+	private final HtmlTextInput startDateField;
+	private final HtmlTextInput endDateField;
+
+	public IntervalController(HtmlHiddenField hiddenField, DateFormat dateFormat, HtmlTextInput startDateField,
+		HtmlTextInput endDateField) {
+	    this.valueField = hiddenField;
+	    this.dateFormat = dateFormat;
+	    this.startDateField = startDateField;
+	    this.endDateField = endDateField;
+	}
+
+	@Override
+	public void execute(IViewState viewState) {
+	    try {
+
+		Date startDate = dateFormat.parse(startDateField.getValue());
+		Date endDate = dateFormat.parse(endDateField.getValue());
+
+		String value = String.format("%s;%s", startDate.getTime(), endDate.getTime());
+
+		this.valueField.setValue(value);
+	    } catch (Exception e) {
+		this.valueField.setValue(DateTimeConverter.INVALID);
+	    }
+	}
+
+    }
+
+}
---
+++ fenix-web-framework/src/java/pt/ist/fenixWebFramework/rendererExtensions/IntervalAsDatesInputRendererWithPicker.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package pt.ist.fenixWebFramework.rendererExtensions;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+import org.apache.commons.collections.Predicate;
+
+import pt.ist.fenixWebFramework.renderers.components.HtmlComponent;
+import pt.ist.fenixWebFramework.renderers.components.HtmlContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlInlineContainer;
+import pt.ist.fenixWebFramework.renderers.components.HtmlScript;
+import pt.ist.fenixWebFramework.renderers.components.HtmlTextInput;
+import pt.ist.fenixWebFramework.renderers.layouts.Layout;
+import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+
+/**
+ * @author Joao Carvalho (joao.pedro.carvalho@ist.utl.pt)
+ * 
+ */
+public class IntervalAsDatesInputRendererWithPicker extends IntervalAsDatesInputRenderer {
+
+    private String image;
+
+    public String getImage() {
+	return image;
+    }
+
+    public void setImage(String image) {
+	this.image = image;
+    }
+
+    @Override
+    protected Layout getLayout(Object object, Class type) {
+	return new IntervalLayoutWithPicker(new SimpleDateFormat(getDateFormat(), getLocale()));
+    }
+
+    public class IntervalLayoutWithPicker extends IntervalLayout {
+
+	/**
+	 * @param simpleDateFormat
+	 */
+	public IntervalLayoutWithPicker(DateFormat simpleDateFormat) {
+	    super(simpleDateFormat, getStartLabel(), getEndLabel());
+	}
+
+	@Override
+	public HtmlComponent createComponent(Object object, Class type) {
+	    HtmlComponent originalComponent = super.createComponent(object, type);
+
+	    List<HtmlComponent> components = originalComponent.getChildren();
+
+	    for (HtmlComponent component : components) {
+		if (component instanceof HtmlContainer) {
+
+		    HtmlTextInput field = (HtmlTextInput) component.getChild(new Predicate() {
+
+			@Override
+			public boolean evaluate(Object field) {
+			    return (field instanceof HtmlTextInput);
+			}
+		    });
+
+		    ((HtmlContainer) component).addChild(getCalendarScript(field.getName()));
+
+		}
+	    }
+
+	    return originalComponent;
+	}
+
+	protected HtmlInlineContainer getCalendarScript(String inputId) {
+	    HtmlInlineContainer container = new HtmlInlineContainer();
+
+	    String scriptText = "$(function() { $(\"input[name='" + RenderUtils.escapeId(inputId)
+		    + "']\").datepicker({showOn: 'button', buttonImage: '" + getImage()
+		    + "', buttonImageOnly: true, firstDay: 1, currentText: '"
+		    + RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.datePicker.currentText") + "', monthNames: "
+		    + RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.datePicker.monthNames")
+		    + ", monthNamesShort: "
+		    + RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.datePicker.monthNamesShort")
+		    + ", dayNamesShort: "
+		    + RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.datePicker.dayNamesShort")
+		    + ", dayNamesMin: " + RenderUtils.getResourceString("RENDERER_RESOURCES", "renderers.datePicker.dayNamesMin")
+		    + ", dateFormat: '" + getInputFormatForCalendar() + "'});});";
+
+	    HtmlScript calendarScript = new HtmlScript();
+	    calendarScript.setContentType("text/javascript");
+	    calendarScript.setScript(scriptText);
+	    container.addChild(calendarScript);
+
+	    return container;
+	}
+
+	protected String getInputFormatForCalendar() {
+	    Locale locale = getLocale();
+	    SimpleDateFormat format = new SimpleDateFormat(getDateFormat(), locale);
+
+	    Calendar c = Calendar.getInstance();
+
+	    c.set(Calendar.YEAR, 1999);
+	    c.set(Calendar.MONTH, 11);
+	    c.set(Calendar.DAY_OF_MONTH, 24);
+
+	    String dateStringFormatted = format.format(c.getTime());
+	    dateStringFormatted = dateStringFormatted.replace("1999", "yy");
+	    dateStringFormatted = dateStringFormatted.replace("99", "y");
+	    dateStringFormatted = dateStringFormatted.replace("12", "mm");
+	    dateStringFormatted = dateStringFormatted.replace("24", "dd");
+
+	    return dateStringFormatted;
+	}
+
+    }
+
+}
