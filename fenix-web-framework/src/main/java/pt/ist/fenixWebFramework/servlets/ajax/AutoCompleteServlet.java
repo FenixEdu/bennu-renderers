c83505e82c78468d87204938b75bec1808628d34
João Antunes <joao.antunes@tagus.ist.utl.pt>
2013-06-04 19:36:24 +0100

    Fixed a bug on the autocomplete

--- fenix-web-framework/src/main/java/pt/ist/fenixWebFramework/servlets/ajax/AutoCompleteServlet.java
+++ fenix-web-framework/src/main/java/pt/ist/fenixWebFramework/servlets/ajax/AutoCompleteServlet.java
@@ -17,9 +17,22 @@ import org.apache.commons.lang.CharEncoding;
 import org.apache.commons.lang.StringUtils;
 
 import pt.ist.fenixWebFramework.renderers.utils.RenderUtils;
+import pt.ist.fenixWebFramework.servlets.filters.contentRewrite.GenericChecksumRewriter;
+import pt.ist.fenixWebFramework.servlets.filters.contentRewrite.RequestChecksumFilter;
+import pt.ist.fenixWebFramework.servlets.filters.contentRewrite.RequestChecksumFilter.UrlTamperingException;
 import pt.ist.fenixWebFramework.servlets.json.JsonObject;
 
 public abstract class AutoCompleteServlet extends HttpServlet {
+
+    public static final String STYLE_CLASS = "styleClass";
+
+    public static final String VALUE_FIELD = "valueField";
+
+    public static final String FORMAT = "format";
+
+    public static final String LABEL_FIELD = "labelField";
+
+    public static final String MAX_COUNT = "maxCount";
 
     private static final String JAVASCRIPT_LIBRARY_ENCODING = CharEncoding.UTF_8;
 
@@ -27,82 +40,124 @@ public abstract class AutoCompleteServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	process(request, response);
+        process(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	process(request, response);
+        process(request, response);
     }
 
     private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	request.setCharacterEncoding(JAVASCRIPT_LIBRARY_ENCODING);
+        request.setCharacterEncoding(JAVASCRIPT_LIBRARY_ENCODING);
 
-	String value = new String(request.getParameter("value").getBytes(), JAVASCRIPT_LIBRARY_ENCODING);
-	Map<String, String> argsMap = getArgsMap(request.getParameter("args"));
-	int maxCount = getNumber(request.getParameter("maxCount"), DEFAULT_MAX_COUNT);
+        //let's take care of the checksum validation
+        try {
+            validateChecksum(request);
 
-	Collection result = getSearchResult(argsMap, value, maxCount);
+        } catch (UrlTamperingException ex) {
+            if (request.getSession() != null) {
+                request.getSession().invalidate();
+            }
+            response.setContentType("application/json; charset=" + JAVASCRIPT_LIBRARY_ENCODING);
+            response.sendError(response.SC_BAD_REQUEST);
+            return;
+        }
 
-	String labelField = request.getParameter("labelField");
-	String format = request.getParameter("format");
-	String valueField = request.getParameter("valueField");
-	String styleClass = request.getParameter("styleClass");
+        String value = new String(request.getParameter("value").getBytes(), JAVASCRIPT_LIBRARY_ENCODING);
+        Map<String, String> argsMap = getArgsMap(request.getParameter("args"));
+        int maxCount = getNumber(request.getParameter(MAX_COUNT), DEFAULT_MAX_COUNT);
 
-	response.setContentType("application/json; charset=" + JAVASCRIPT_LIBRARY_ENCODING);
-	response.getWriter().write(getResponseHtml(result, labelField, format, valueField, styleClass, maxCount));
+        Collection result = getSearchResult(argsMap, value, maxCount);
+
+        String labelField = request.getParameter(LABEL_FIELD);
+        String format = request.getParameter(FORMAT);
+        String valueField = request.getParameter(VALUE_FIELD);
+        String styleClass = request.getParameter(STYLE_CLASS);
+
+        response.setContentType("application/json; charset=" + JAVASCRIPT_LIBRARY_ENCODING);
+        response.getWriter().write(getResponseHtml(result, labelField, format, valueField, styleClass, maxCount));
+    }
+
+    private void validateChecksum(HttpServletRequest request) throws RequestChecksumFilter.UrlTamperingException {
+        String checksum = request.getParameter(GenericChecksumRewriter.CHECKSUM_ATTRIBUTE_NAME);
+        if (checksum == null || checksum.length() == 0) {
+            checksum = (String) request.getAttribute(GenericChecksumRewriter.CHECKSUM_ATTRIBUTE_NAME);
+        }
+
+        if (!isValidChecksum(request, checksum)) {
+            throw new RequestChecksumFilter.UrlTamperingException();
+        }
+
+    }
+
+    private boolean isValidChecksum(final HttpServletRequest request, final String checksum) {
+        //let's just extract the meaningful/important parameters. i.e. the ones
+        //parsed to the getResponseHtml
+        String checksumRelevantString = "";
+        checksumRelevantString += request.getParameter(MAX_COUNT);
+        checksumRelevantString += request.getParameter(LABEL_FIELD);
+        checksumRelevantString += request.getParameter(FORMAT);
+        checksumRelevantString += request.getParameter(VALUE_FIELD);
+        checksumRelevantString += request.getParameter(STYLE_CLASS);
+
+        return checksum.length() > 0
+                && (checksum.equals(GenericChecksumRewriter.calculateChecksum(checksumRelevantString)) || checksum
+                        .equals(GenericChecksumRewriter.calculateChecksum(RequestChecksumFilter.decodeURL(checksumRelevantString,
+                                JAVASCRIPT_LIBRARY_ENCODING))));
+
     }
 
     private int getNumber(String parameter, int defaultValue) {
-	if (parameter == null) {
-	    return defaultValue;
-	}
+        if (parameter == null) {
+            return defaultValue;
+        }
 
-	try {
-	    return Integer.parseInt(parameter);
-	} catch (NumberFormatException e) {
-	    return defaultValue;
-	}
+        try {
+            return Integer.parseInt(parameter);
+        } catch (NumberFormatException e) {
+            return defaultValue;
+        }
     }
 
     protected abstract Collection getSearchResult(Map<String, String> argsMap, String value, int maxCount);
 
     private Map<String, String> getArgsMap(String encodedServiceArgs) {
-	String[] serviceArgsArray = StringUtils.split(encodedServiceArgs, ',');
-	Map<String, String> serviceArgsMap = new HashMap<String, String>();
+        String[] serviceArgsArray = StringUtils.split(encodedServiceArgs, ',');
+        Map<String, String> serviceArgsMap = new HashMap<String, String>();
 
-	for (String serviceArg : serviceArgsArray) {
-	    String[] argNameArgValue = StringUtils.split(serviceArg, '=');
-	    serviceArgsMap.put(argNameArgValue[0], argNameArgValue[1]);
-	}
+        for (String serviceArg : serviceArgsArray) {
+            String[] argNameArgValue = StringUtils.split(serviceArg, '=');
+            serviceArgsMap.put(argNameArgValue[0], argNameArgValue[1]);
+        }
 
-	return serviceArgsMap;
+        return serviceArgsMap;
     }
 
     private String getResponseHtml(Collection result, String labelField, String format, String valueField, String styleClass,
-	    int maxCount) {
-	List<JsonObject> jsonObjects = new ArrayList<JsonObject>();
-	try {
-	    int count = 0;
-	    for (final Object element : result) {
-		if (count++ >= maxCount) {
-		    break;
-		}
+            int maxCount) {
+        List<JsonObject> jsonObjects = new ArrayList<JsonObject>();
+        try {
+            int count = 0;
+            for (final Object element : result) {
+                if (count++ >= maxCount) {
+                    break;
+                }
 
-		final String labelProperty = BeanUtils.getProperty(element, labelField);
-		if (format == null) {
-		    jsonObjects.add(new JsonObject(BeanUtils.getProperty(element, valueField), labelProperty));
-		} else {
-		    jsonObjects.add(new JsonObject(BeanUtils.getProperty(element, valueField), RenderUtils
-			    .getFormattedProperties(format, element)));
-		}
-	    }
-	} catch (Exception ex) {
-	    throw new RuntimeException("Error getting field property (see label and value fields)", ex);
+                final String labelProperty = BeanUtils.getProperty(element, labelField);
+                if (format == null) {
+                    jsonObjects.add(new JsonObject(BeanUtils.getProperty(element, valueField), labelProperty));
+                } else {
+                    jsonObjects.add(new JsonObject(BeanUtils.getProperty(element, valueField), RenderUtils
+                            .getFormattedProperties(format, element)));
+                }
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Error getting field property (see label and value fields)", ex);
 
-	}
+        }
 
-	return JsonObject.getJsonArrayString(jsonObjects);
+        return JsonObject.getJsonArrayString(jsonObjects);
     }
 }
