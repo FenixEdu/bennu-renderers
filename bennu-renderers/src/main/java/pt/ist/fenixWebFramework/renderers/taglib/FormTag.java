745cfe64650cffe03ad9f3503c8ab9a602658b90
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2014-06-16 16:01:39 +0100

    Removed hard-coded references to static resources
      * Also added JSTL dependency

--- bennu-renderers/src/main/java/pt/ist/fenixWebFramework/renderers/taglib/FormTag.java
+++ bennu-renderers/src/main/java/pt/ist/fenixWebFramework/renderers/taglib/FormTag.java
@@ -142,13 +142,13 @@ public class FormTag extends ContextTag {
             String path = ((HttpServletRequest) pageContext.getRequest()).getContextPath();
             formHead.append("<script type=\"text/javascript\" src=\"");
             formHead.append(path);
-            formHead.append("/javaScript/jquery.ui.draggable.js\"></script>\n");
+            formHead.append("/bennu-renderers/js/jquery.ui.draggable.js\"></script>\n");
             formHead.append("<script type=\"text/javascript\" src=\"");
             formHead.append(path);
-            formHead.append("/javaScript/jquery.alerts.js\"></script>\n");
+            formHead.append("/bennu-renderers/js/jquery.alerts.js\"></script>\n");
             formHead.append("<script type=\"text/javascript\" src=\"");
             formHead.append(path);
-            formHead.append("/javaScript/alertHandlers.js\"></script>\n");
+            formHead.append("/bennu-renderers/js/alertHandlers.js\"></script>\n");
         }
 
         formHead.append("<form ");
@@ -173,7 +173,6 @@ public class FormTag extends ContextTag {
         if (getStyle() != null) {
             formHead.append("style=\"" + getStyle() + "\" ");
         }
-
 
         formHead.append("method=\"post\" class='fenix-rendered'>\n");
         try {
