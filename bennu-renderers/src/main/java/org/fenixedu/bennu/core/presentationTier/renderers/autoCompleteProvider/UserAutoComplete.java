775b1816946fb118da2bbf830192096426ab5a4a
AngelaFrancisca <angela.almeida@tecnico.ulisboa.pt>
2016-05-31 19:32:41 +0100

    Upgrade to bennu 4 and make code compatible.

--- bennu-renderers/src/main/java/org/fenixedu/bennu/core/presentationTier/renderers/autoCompleteProvider/UserAutoComplete.java
+++ bennu-renderers/src/main/java/org/fenixedu/bennu/core/presentationTier/renderers/autoCompleteProvider/UserAutoComplete.java
@@ -64,10 +64,11 @@ public class UserAutoComplete implements AutoCompleteProvider<User> {
         Set<User> users = new HashSet<User>();
         String[] values = StringNormalizer.normalize(value).split(" ");
         for (User user : Bennu.getInstance().getUserSet()) {
-            final String normalizedUser = StringNormalizer.normalize(user.getPresentationName());
-
-            if (hasMatch(values, normalizedUser)) {
-                users.add(user);
+            if (user.getProfile() != null) {
+                final String normalizedUser = StringNormalizer.normalize(user.getDisplayName());
+                if (hasMatch(values, normalizedUser)) {
+                    users.add(user);
+                }
             }
         }
         return users;
